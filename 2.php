cb10f8d5a2fa5e289d0c790325d4ba3ef1b429e7
Đàm Phương Anh <bushigaoshisi@gmail.com>
2024-09-11 23:00:47 +0700

    Add files via upload

---
+++ 2.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset=""UTF-8">
+    <meta name=""viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+echo "GenG la nha vo dich LCK 4 ki lien tiep";
+echo "<br>";
+$x = "Dam Phuong Anh";
+echo "Độ dài chuỗi:". strlen($x),"<br>";
+
+?>
